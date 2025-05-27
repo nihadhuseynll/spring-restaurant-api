04540ad88aa6d54ebbd284e9d8be155bab751ba3
nihadhuseynll <huseynlinihad4400@gmail.com>
2025-05-27 11:36:55 +0400

    Initial commit

---
+++ src/main/java/com/codex/springrestaurantapi/SpringRestaurantApiApplication.java
@@ -0,0 +1,13 @@
+package com.codex.springrestaurantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestaurantApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestaurantApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codex/springrestaurantapi/SpringRestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codex.springrestaurantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestaurantApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
